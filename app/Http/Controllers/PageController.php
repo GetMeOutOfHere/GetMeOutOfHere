00b6c881ad14018b6d4a2002e0471f3bc5e53028
Siya <siya.nyinfo@gmail.com>
2017-10-21 20:39:43 +0700

    change from m to mi

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -23,19 +23,19 @@ class PageController extends Controller
     public function scheduleSms(Request $request){
         if(Auth::user()){
             if($request->isMethod('post')){
-                $send_time = $request->input('send_time');
                 $fake_sender_name = $request->input('fake_sender_name');
                 $fake_sender_number = $request->input('fake_sender_number');
                 $send_to = $request->input('send_to');
                 $message = $request->input('message');
                 $send_time = $request->input('send_time');
-                list($y, $m, $d, $h, $m, $s) = explode("/", $send_time);
+
+                list($y, $m, $d, $h, $mi, $s) = explode("/", $send_time);
                 $sms = SMSQueue::create([
                     'fake_sender_name' => $fake_sender_name,
                     'fake_sender_number' => $fake_sender_number,
                     'send_to' => $send_to,
                     'message' => $message,
-                    'send_time' => Carbon::create($y, $m, $d, $h, $m, $s, "Asia/Phnom_Penh")
+                    'send_time' => Carbon::create($y, $m, $d, $h, $mi, $s, "Asia/Phnom_Penh")
                 ]);
 
                 if($sms){
