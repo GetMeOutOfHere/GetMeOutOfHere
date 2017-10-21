8669d188f78a01d7ff42f1727bd0baaa1f796998
Marcus Holmes <mh@marcusholmes.biz>
2017-10-21 17:47:09 +0700

    change text on front page

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -24,9 +24,9 @@
         <div class="section no-pad-bot">
           <div class="container">
             <br><br>
-            <h1 class="header center white-text banner-title">Welcome! to Get Me Out of Here platform</h1>
+            <h1 class="header center white-text banner-title">Get A Great Excuse When You Need It Most</h1>
             <div class="row center">
-              <h5 class="header col s12 white-text light banner-description">Help yourself get out of an unwanted situation with a comfortable reason from us! Feel Free to try our awesome features now! :)</h5>
+              <h5 class="header col s12 white-text light banner-description">Stuck In An Awkward Social Situation?  <br> Have A Reason To Leave Texted To Your Phone</h5>
             </div>
             <div class="row center">
               <a href="http://materializecss.com/getting-started.html" id="sign-up-button" class="btn-large waves-effect waves-light red darken-3">Sign Up Now!</a>
@@ -42,32 +42,37 @@
         <div class="section" id="how-it-work">
           <div class="row">
             <div class="col s12">
-              <h1 class="header center red-text text-darken-3 how-it-work-title">How It Work
+              <h1 class="header center red-text text-darken-3 how-it-work-title">How It Works
             </div>
             <div class="col s12 m4">
               <div class="icon-block">
                 <h2 class="center brown-text"><i class="material-icons">flash_on</i></h2>
-                <h5 class="center">Step 1</h5>
+                <h5 class="center">Sign Up</h5>
 
-                <p class="light">We did most of the heavy lifting for you to provide a default stylings that incorporate our custom components. Additionally, we refined animations and transitions to provide a smoother experience for developers.</p>
+                <p class="light">Tell us who you are, and importantly, what your phone number is.</p>
+                <p class="light">Add our number to your phone. Call us "Mum" or "Hubby".</p>
               </div>
             </div>
 
             <div class="col s12 m4">
               <div class="icon-block">
                 <h2 class="center brown-text"><i class="material-icons">group</i></h2>
-                <h5 class="center">Step 2</h5>
+                <h5 class="center">Set Up</h5>
 
-                <p class="light">By utilizing elements and principles of Material Design, we were able to create a framework that incorporates components and animations that provide more feedback to users. Additionally, a single underlying responsive system across all platforms allow for a more unified user experience.</p>
+                <p class="light">Tell us what to text you with, and when to text you. Get Imaginative!</p>
               </div>
             </div>
 
             <div class="col s12 m4">
               <div class="icon-block">
                 <h2 class="center brown-text"><i class="material-icons">settings</i></h2>
-                <h5 class="center">Step 3</h5>
+                <h5 class="center">Get Out</h5>
 
-                <p class="light">We have provided detailed documentation as well as specific code examples to help new users get started. We are also always open to feedback and can answer any questions a user may have about Materialize.</p>
+                <p class="light">Get your text.</p>
+                <p class="light">Look startled.</p>
+                <p class="light">Show them the text.</p>
+                <p class="light">Get the hell out.</p>
+                
               </div>
             </div>
           </div>
