552efee35711b6be342e3ce02ae79e75478b5803
Siya <siya.nyinfo@gmail.com>
2017-10-22 11:04:03 +0700

    add funny random message to sms queue

--- resources/assets/js/partials/sms_queue.js
+++ resources/assets/js/partials/sms_queue.js
@@ -18,6 +18,21 @@ $(document).ready(function() {
     ampmclickable: true
   });
 
+  $('#rendom-button').click(function() {
+    rendomNumber = Math.floor((Math.random() * 7) + 1);
+    arraySms =
+    ["COME HOME NOW!!! Your brother did the thing again. I NEED YOU!!!",
+    "I found the thing. I'm so disappointed in you. Come home now! I mean it!😡😲😠",
+    "Hey honey, I need you to come over right now... I don't have to explain why xxx 💕❤️",
+    "OMG COME HOME! There's a guy here who says he's your cousin and wants the money back!!!",
+    "PLEASE HELP! I got stuck in the thing again! Come round now!😮😬😱",
+    "Babe. I need you. Right now. Come home 😍❤️❤️",
+    "There's a policeman here with a problem. I think you need to come home right now...!",
+    "Sir, I have found your wallet. Please meet me at the police station"];
+
+    $('#message').val(arraySms[rendomNumber]);
+  });
+
   $('#submit-sms-queue').click(function() {
     scheduleDate = $('#schedule-date').val();
     scheduleTime = $('#schedule-time').val();
@@ -29,5 +44,5 @@ $(document).ready(function() {
     minute       = dateObject.getMinutes();
 
     $('#set-schedule').val(year + '/' + month + '/' + day + '/' + hour + '/' + minute + '/0' );
-  })
+  });
 });
--- resources/views/layouts/sms_queue.blade.php
+++ resources/views/layouts/sms_queue.blade.php
@@ -33,8 +33,8 @@
     <div class="row">
       <div class="input-field col s12 m6 push-m3">
         <i class="material-icons prefix">sms</i>
-        <textarea id="message" class="materialize-textarea" name="message"></textarea>
-        <label for="message">Message</label>
+        <textarea name="message" id="message" disabled  class="materialize-textarea"></textarea>
+        <span class="btn" id="rendom-button">Generate Message</span>
       </div>
     </div>
     <div class="row">
